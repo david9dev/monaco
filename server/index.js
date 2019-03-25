43a1a27f76a5e3222a95c1dab9d2330a1232eddf
David Wells <davidwells@Davids-MacBook-Pro.local>
2019-03-25 14:03:08 -0600

    testing sockets

--- server/index.js
+++ server/index.js
@@ -1,8 +1,24 @@
 const express = require('express');
 const app = express();
 const {json} = require('express');
-
 app.use(json());
 app.use(express.static(`${__dirname}/../build`))
+let text = "";
 
-app.listen(4000,() => console.log('port', 4000))
+const server = app.listen(4000,() => console.log('port', 4000))
+const io = require('socket.io')(server);
+
+io.on('connection', (socket) =>
+{
+    console.log('user connected');
+    io.emit('message dispatched', 'hello');
+    io.emit('text dispatch', text);
+    socket.on('update text',(stext) =>
+    {
+        let text = stext;
+        // console.log(text);
+        socket.broadcast.emit('text dispatch', text);
+    })
+})
+
+
--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import io from 'socket.io-client';
 // import { render } from 'react-dom';
 import MonacoEditor from 'react-monaco-editor';
 
@@ -7,14 +8,35 @@ class App extends React.Component {
     super(props);
     this.state = {
       code: '// type your code...',
+      socket: io.connect('/')
     }
   }
+  componentDidMount()
+  {
+    this.socket = io.connect('/')
+    // console.log(socket);
+    this.socket.on('message dispatched', (data) =>
+    {
+      console.log(data)
+    })
+    this.socket.on('text dispatch', (text) =>
+    {
+      console.log(text)
+      this.setState({code: text});
+    })
+    
+  }
+
   editorDidMount(editor, monaco) {
     console.log('editorDidMount', editor);
     editor.focus();
   }
-  onChange(newValue, e) {
-    console.log('onChange', newValue, e);
+  onChange(newValue, e,) {
+    // const {socket} = this.state.socket
+    this.socket.emit('update text', newValue);
+    // console.log(this.state)
+    // console.log('onChange', newValue, e);
+    
   }
   render() {
     const code = this.state.code;
@@ -22,16 +44,19 @@ class App extends React.Component {
       selectOnLineNumbers: true
     };
     return (
-      <MonacoEditor
-        width="800"
-        height="600"
-        language="json"
-        theme="vs-dark"
-        value={code}
-        options={options}
-        onChange={this.onChange}
-        editorDidMount={this.editorDidMount}
-      />
+      <div>
+        <MonacoEditor
+          width="800"
+          height="600"
+          language="javascript"
+          theme="vs-dark"
+          value={code}
+          options={options}
+          onChange={(newValue, e) => this.onChange(newValue, e)}
+          editorDidMount={this.editorDidMount}
+        />
+        <button onClick={() => this.yeet()}>click</button>
+      </div>
     );
   }
 }
